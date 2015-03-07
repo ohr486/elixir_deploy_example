deafdb90b0b5bf597f916592361d36408bee9998
ohr486 <ohr486@gmail.com>
2015-03-07 17:37:58 +0900

    implement deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,3 @@
-# For help in making your deploy script, see the Mina documentation:
-#
-#  - http://nadarei.co/mina
-#  - http://nadarei.co/mina/tasks
-#  - http://nadarei.co/mina/settings
-#  - http://nadarei.co/mina/helpers
-
 #require 'mina/bundler'
 require 'mina/git'
 require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
@@ -20,6 +13,7 @@ set :domain, ENV['exdomain']
 set :deploy_to, '/var/www/elixir'
 set :repository, 'git://github.com/ohr486/elixir_deploy_example.git'
 set :branch, 'master'
+set :app_name, 'elixir_deploy_example'
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
@@ -27,9 +21,10 @@ set :shared_paths, ['log']
 
 # Optional settings:
 set :user, 'deploy-user'    # Username in the server to SSH to.
-#set :port, '30000'     # SSH port number.
 set :forward_agent, true     # SSH forward_agent.
 set :identity_file, '/Users/ooharatsunenori/ohr-ec2-test.pem'
+
+elixir_commands = [:start, :stop, :ping]
 
 # This task is the environment that is loaded for most commands, such as
 # `mina deploy` or `mina rake`.
@@ -40,6 +35,7 @@ task :environment do
 
   # For those using RVM, use this to load an RVM version@gemset.
   # invoke :'rvm:use[ruby-1.9.3-p125@default]'
+  queue %[source /etc/profile.d/kerl.sh]
 end
 
 # Put any custom mkdir's in here for when `mina setup` is ran.
@@ -48,12 +44,6 @@ end
 task :setup => :environment do
   queue! %[sudo mkdir -p "#{deploy_to}/#{shared_path}/log"]
   queue! %[sudo chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/log"]
-
-#  queue! %[sudo mkdir -p "#{deploy_to}/#{shared_path}/config"]
-#  queue! %[sudo chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/config"]
-
-#  queue! %[touch "#{deploy_to}/#{shared_path}/config/database.yml"]
-#  queue  %[echo "-----> Be sure to edit '#{deploy_to}/#{shared_path}/config/database.yml'."]
 end
 
 desc "Deploys the current version to the server."
@@ -63,11 +53,21 @@ task :deploy => :environment do
     # instance of your project.
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
-    queue! %[mix deps.get]
+    queue %[mix deps.get]
+    queue %[mix release]
 
     to :launch do
       #queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
       #queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
+    end
+  end
+end
+
+namespace :elixir do
+  elixir_commands.each do |cmd|
+    desc "elixir command[#{cmd}]"
+    task cmd => :environment do
+      queue "cd #{deploy_to}/#{current_path}/rel/#{app_name}/bin && ./#{app_name} #{cmd}"
     end
   end
 end
